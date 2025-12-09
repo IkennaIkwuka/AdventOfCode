0042cf7f7ada6f3a1bc0160555676332f8f1daf3
Ikenna Ikwuka <ikenna.n.ikwuka@gmail.com>
2025-12-09 15:55:13 +0100

    completed part 1 of day 1

--- 2025/src/01/main.py
+++ 2025/src/01/main.py
@@ -8,11 +8,11 @@ def run():
     dial_min, dial_max = 0, 99
     password = 0
 
-    with open("2025/src/01/sample.txt", "r") as f:
+    with open("2025/src/01/input.txt", "r") as f:
         lines = [x.strip() for x in f.readlines()]
 
     if DEBUG:
-        print(f"The dial starts by pointing at {dial}.\n")
+        print(f"\nThe dial starts by pointing at {dial}.")
 
     for instruction in lines:
         direction = instruction[0]
@@ -37,13 +37,10 @@ def run():
         if dial == 0:
             password += 1
             if DEBUG:
-                print(f"\nDial at {dial}, registering occurrence...\n")
+                print(f"Dial at {dial}, registering occurrence...\n")
 
-        if DEBUG:
-            print()
 
-    print(f"Final password: {password}")
-    return password
+    return f"Final password: {password}"
 
 
 if __name__ == "__main__":
